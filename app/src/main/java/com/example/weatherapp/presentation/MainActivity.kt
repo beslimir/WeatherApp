0247db20994e2ec07b478a6b48e3527e8d6e8e63
beslimir <marin.beslo@gmail.com>
2022-08-01 09:29:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.weatherapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatherapp.presentation.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WeatherAppTheme {
+        Greeting("Android")
+    }
+}
